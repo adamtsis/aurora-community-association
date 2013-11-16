86c65e0c635b30f940a44467e319edf1b6dfdfdb
Anthony Damtsis <adamtsis@dius.com.au>
2013-11-16 23:42:48 +1100

    Added fire watch to easy menu

--- wp-content/themes/aca2011/easy_menu_detail.php
+++ wp-content/themes/aca2011/easy_menu_detail.php
@@ -5,8 +5,8 @@
         <td class="align-middle align-left"><a href="<?php echo site_url('wp-login.php', 'login') ?>">Member login<br />&amp; Registration</a></td>
         <td class="align-middle align-right"><a href="<?php echo get_site_url().'/forum' ?>"><img src="<?php bloginfo('template_url') ?>/images/user_group_icon.png" alt="Onlone Forum"/></a></td>
         <td class="align-middle align-left"><a href="<?php echo get_site_url().'/forum' ?>">Community<br />Online Forum</a></td>
-        <td class="align-middle align-right"><a href="#"><img src="<?php bloginfo('template_url') ?>/images/torch_icon.png" alt="Spot Light"/></a></td>
-        <td class="align-middle align-left"><a href="<?php get_category_link_by_name('Community Spotlight'); ?>">&nbsp;Community<br />&nbsp;Spotlight</a></td>
+        <td class="align-middle align-right"><a href="#"><img src="<?php bloginfo('template_url') ?>/images/torch_icon.png" alt="Fire Watch"/></a></td>
+        <td class="align-middle align-left"><a href="http://www.aurora.asn.au/fire-watch">&nbsp;Fire Watch</a></td>
         <td class="align-middle align-right"><a href="http://www.aurora.asn.au/wp-login.php?action=register"><img src="<?php bloginfo('template_url') ?>/images/email_icon.png" alt="News Letter Sign Up"/></a></td>
         <td class="align-middle align-left"><a href="http://www.aurora.asn.au/wp-login.php?action=register">&nbsp;Newsletter<br />&nbsp;Sign Up</a></td>
     </tr>
